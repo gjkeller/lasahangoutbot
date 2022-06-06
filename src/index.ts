0060f156ad05bd7f990480a76dbc02943d3b2fb9
Gabriel Keller <gabrieljameskeller@gmail.com>
2022-06-06 01:07:04 -0500

    Get bot working

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import { Client } from 'discord.js'
+import { Client, Intents } from 'discord.js'
+import config from '../config.json' assert { type: 'json' }
 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
+client.once('ready', () => {
+  console.log('Bot is ready!')
+})
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return
+
+  const { commandName } = interaction
+
+  if (commandName === 'areyousentient') {
+    await interaction.reply('Maybe..?')
+  }
+})
+
+client.login(config.token)
--- src/register-commands.ts
+++ src/register-commands.ts
@@ -1,2 +1,15 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
 import { REST } from '@discordjs/rest'
 import { Routes } from 'discord-api-types/v9'
+import config from '../config.json' assert { type: 'json' }
+
+const commands = [
+  new SlashCommandBuilder().setName('areyousentient').setDescription('Is LASABot sentient?')
+]
+  .map(command => command.toJSON())
+
+const rest = new REST({ version: '10' }).setToken(config.token)
+
+rest.put(Routes.applicationGuildCommands('624434172773138474', config.guildId), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error)
